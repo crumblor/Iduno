16b919148c792fd1c435bacfbeb45b89e09578a6
Roberts Gulbis <rgubis@Gubis-2.local>
2024-12-12 10:39:08 +0200

    css

--- migration.sql
+++ migration.sql
@@ -17,3 +17,24 @@ VALUES
 
 SELECT * FROM posts;
 
+INSERT INTO posts (content)
+VALUES
+    ("Kas notiek patiesi?"),
+    ("Laiks doties uz Rigu!"),
+    ("Neviens neklausas!"),
+    ("Es tikai gribu mieru..."),
+    ("Kur pazuda sniegs?"),
+    ("Ziemassvetki bez mandariniem..."),
+    ("Cikos saksies balite?"),
+    ("Nav laika, nav naudas."),
+    ("Atdod manu cepuri, Leons!"),
+    ("Sniegavirs ir netaisits."),
+    ("Laiks ir tikai iluzija."),
+    ("Kas par jauniem draugiem?"),
+    ("Mans kaimins ir traks."),
+    ("Ko tu dari tur?"),
+    ("Vel viens tas pats."),
+    ("Zvaigznes virs manas galvas."),
+    ("Pirmais janvaris ir tuvuma.");
+   
+   
