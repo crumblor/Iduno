b5432c4a34245722d455252fdfa2e9780b23cbb6
Roberts Gulbis <rgubis@Gubis-2.local>
2025-01-07 12:31:56 +0200

    sodiena

--- index.php
+++ index.php
@@ -8,8 +8,6 @@
 </head>
 <body>
 
-
-
 <?php
     require "functions.php";
     require "Database.php";
@@ -18,6 +16,17 @@
     $db = new Database($config["database"]);
 
     $posts = $db->query("SELECT * FROM posts")->fetchAll();
+
+    if(isset($_GET["search_query"]) && ($_GET["search_query"]) !== "") {
+        $posts = $db->query("SELECT * FROM  posts WHERE content LIKE  '%" . $_GET["search_query"]. "%'")->fetchAll();
+    }
+
+    echo "<h1>Blooooogs</h1>";
+
+    echo "<form>";
+        echo '<input name="search_query"/>';
+        echo '<button>🔍🔎</button>';
+    echo "</form>";
 
     echo "<ul>";
     foreach($posts as $post) {
