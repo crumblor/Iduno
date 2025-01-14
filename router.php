65cdcf8b86436afe716fd1650837b050e1b7ce63
Roberts Gulbis <rgubis@Gubis-2.local>
2025-01-14 13:30:21 +0200

    errors

--- router.php
+++ router.php
@@ -1,13 +1,11 @@
 <?php
 $uri = parse_url($_SERVER["REQUEST_URI"])["path"];
+$routes = require("routes.php");
 
-if ($uri == "/") {
-    require "controllers/index.php";
-} elseif ($uri == "/about") {
-    require "controllers/story.php";
-} elseif ($uri == "/categories") {
-    require "controllers/categories.php";
+if (array_key_exists($uri, $routes)) {
+    require $routes[$uri];
 } else {
     http_response_code(404);
+    require "controllers/404.php";
     die();
 }
---
+++ views/404.views.php
@@ -0,0 +1,7 @@
+<?php $title = "404";?>
+<?php $css = "css/styles_v3.css";?>
+<?php require "components/header.php";?>
+<?php require "components/navbar.php";?>
+<h1>Je.. nebus....</h1>
+<?php require "components/footer.php";?>
+    
