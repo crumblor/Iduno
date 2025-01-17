3347612d45fc811b64263d17cd230df857e8683c
Roberts Gulbis <rgubis@Gubis-2.local>
2025-01-17 08:17:53 +0200

    unfunny kautaks lieks if

--- controllers/posts/delete.php
+++ controllers/posts/delete.php
@@ -3,14 +3,12 @@
 require "Validator.php";
 
 if($_SERVER["REQUEST_METHOD"] == "POST") {
-    if(empty($errors)) {
-        $sql = 'DELETE FROM posts
-        WHERE id = :id';
-        $params = ["id" => $_POST["id"]];
-        $post = $db->query($sql, $params)->fetch();
-        header('Location: /'); 
-        exit();
-    }
+    $sql = 'DELETE FROM posts
+    WHERE id = :id';
+    $params = ["id" => $_POST["id"]];
+    $post = $db->query($sql, $params)->fetch();
+    header('Location: /'); 
+    exit();
 } else {
     redirectIfNotFound();
 }
