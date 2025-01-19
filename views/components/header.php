d669cf739c3009d44902e21c97473fbe14605c37
Roberts Gulbis <rgubis@Gubis-2.local>
2025-01-19 18:58:20 +0200

    Update changes

--- views/components/header.php
+++ views/components/header.php
@@ -4,6 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?? "112!!?!?!?!"?></title>
-    <link rel="stylesheet" href="css/styles_v5.css">
+    <link rel="stylesheet" href="css/styles_v6.css">
 </head>
 <body>
