fd7f4d9e2d337b610f9b2a8278eabae838a6f550
Roberts Gulbis <rgubis@Gubis-2.local>
2025-01-30 09:47:43 +0200

    Update changes

---
+++ migration.sql
@@ -0,0 +1,20 @@
+USE blog_ipb23;
+
+DROP DATABASE blog_ipb23;
+
+CREATE DATABASE blog_ipb23;
+
+CREATE TABLE posts (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    content varchar(25) NOT NULL,
+    category_id INT
+);
+
+CREATE TABLE categories (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    category_name VARCHAR(255) NOT NULL
+);
+
+SELECT * from posts;
+
+SELECT * from categories;
