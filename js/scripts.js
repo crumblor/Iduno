ae1e2df4f13808703b27fffb528efae847ec6756
Roberts Gulbis <rgubis@Gubis-2.local>
2025-02-05 14:31:34 +0200

    system def

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
-// const prefersDark = window
-// .matchMedia("(prefers-color-scheme: dark)")
-// .matches;
-// document.body
-// .classList
-// .add(prefersDark ? "dark-mode" : "light-mode");
-
+const themeSwitch = document.getElementById("theme-select")
 const currentTheme = localStorage.getItem("theme");
+
 if (currentTheme === "dark") {
     document.body.classList.add("dark-mode");
-} else {
+} else if (currentTheme === "light") {
     document.body.classList.add("light-mode");
+} else {
+    const prefersDark = window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .matches;
+    document.body
+    .classList
+    .add(prefersDark ? "dark-mode" : "light-mode");
 }
-
-const themeSwitch = document.getElementById("theme-select")
 
 themeSwitch.addEventListener('click', () => {
     const currentTheme = localStorage.getItem("theme");
